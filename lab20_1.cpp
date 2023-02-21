4fde85e5aa2202085cbb4c6bb4862c038ac5a379
kittiwin phannachet <kittiwinktb@gmail.com>
2023-02-21 17:17:29 +0700

    lab20_2complete

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,34 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){
+	double diffx = R1.x - R2.x;
+	if(diffx < 0) diffx *= -1;
 
+	double diffy = R1.y - R2.y;
+	if(diffy < 0) diffy *= -1;
 
+	if(diffx > R1.w || diffx > R2.w) return 0;
+	if(diffy > R1.h || diffy > R2.h) return 0;
+
+	//แกน x ด้านซ้าย
+	double maxX;
+	if(R1.x > R2.x) maxX = R1.x;
+	else maxX = R2.x;
+	//แกน x ด้านขวา
+	double minX;
+	if((R1.x + R1.w) < (R2.x + R2.w)) minX = R1.x + R1.w;
+	else minX = R2.x + R2.w;
+	double Lx = minX - maxX;
+
+	//แกน y บน
+	double maxY;double minY;
+	if(R1.y > R2.y) minY = R2.y;
+	else minY = R1.y;
+	//ด้านล่าง
+	if((R1.y - R1.h) < (R2.y - R2.h)) maxY = R2.y - R2.h;
+	else maxY = R1.y - R1.h;
+	double Ly = minY - maxY;
+	return Lx*Ly;
 
 }
